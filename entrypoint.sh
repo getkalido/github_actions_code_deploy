6d081ab47bef146e9d7428ed67a50362f16a49a5
Bernhard Müller <BWMuller@users.noreply.github.com>
2019-11-05 11:55:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,6 +53,7 @@ text
 EOF
 
 aws deploy create-deployment --profile code-deploy-action \
+              --region ${AWS_REGION}
               --application-name ${AWS_APPLICATION_NAME} \
               --deployment-group-name ${AWS_DEPLOYMENT_GROUP_NAME} \
               --s3-location bucket=${AWS_S3_BUCKET},bundleType=${AWS_S3_BUNDLE_TYPE},key=${AWS_S3_LOCATION_KEY} >> out.json
